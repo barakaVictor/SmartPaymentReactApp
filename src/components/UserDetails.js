037471758f9e965cef5d6b29fa642fded544aa77
Victor Baraka <54454611+barakaVictor@users.noreply.github.com>
2020-07-06 23:17:40 +0300

    Add input handler to user details component Add input handler to update state accordingly as a user types in their card details

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -4,17 +4,57 @@ class UserDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cardDigits: []
+      cardDigits: {
+        firstset:[],
+        secondset:[],
+        thirdset:[],
+        fourthset:[]
+      }
     };
+
+    this.handleInput = this.handleInput.bind(this);
   }
-  componentDidMount() {}
+
+  handleInput(event){
+    let value = event.target.value;
+    this.setState({
+      cardDigits:{
+      ...this.state.cardDigits, [event.target.name]:value
+      }
+    });
+  }
+
   render() {
     return (
       <div className="user-details">
-        <input type="text" size="4" placeholder="----" />
-        <input type="text" size="4" placeholder="----" />
-        <input type="text" size="4" placeholder="----" />
-        <input type="text" size="4" placeholder="----" />
+        <input 
+          type="text" 
+          size="4" 
+          name="firstset" 
+          placeholder="----" 
+          value={this.state.cardDigits.firstset} 
+          onChange={this.handleInput}/>
+        <input 
+          type="text" 
+          size="4" 
+          name="secondset" 
+          placeholder="----" 
+          value={this.state.cardDigits.secondset} 
+          onChange={this.handleInput}/>
+        <input 
+          type="text"  
+          size="4" 
+          name="thirdset" 
+          placeholder="----" 
+          value={this.state.cardDigits.thirdset} 
+          onChange={this.handleInput}/>
+        <input 
+          type="text" 
+          size="4" 
+          name="fourthset" 
+          placeholder="----" 
+          value={this.state.cardDigits.fourthset} 
+          onChange={this.handleInput}/>
       </div>
     );
   }
